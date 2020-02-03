dd039fb3dfc8d97fb56a5acf703e49eebfbc193b
Jeremy Kuhnash <kuhnash@gmail.com>
2020-02-02 17:33:54 -0700

    detect_edges attempt and worskspace

---
+++ detect_edges.py
@@ -0,0 +1,33 @@
+"""
+ * Python script to demonstrate Canny edge detection.
+ *
+ * usage: python CannyEdge.py <filename> <sigma> <low_threshold> <high_threshold>
+"""
+import skimage
+import skimage.feature
+import skimage.viewer
+import sys
+
+print("here we go")
+# read command-line arguments
+filename = sys.argv[1]
+sigma = float(sys.argv[2])
+low_threshold = float(sys.argv[3])
+high_threshold = float(sys.argv[4])
+
+# load and display original image as grayscale
+image = skimage.io.imread(fname=filename, as_gray=True)
+viewer = skimage.viewer(image=image)
+viewer.show()
+
+edges = skimage.feature.canny(
+    image=image,
+    sigma=sigma,
+    low_threshold=low_threshold,
+    high_threshold=high_threshold,
+)
+
+# display edges
+viewer = skimage.viewer.ImageViewer(edges)
+viewer.show()
+
